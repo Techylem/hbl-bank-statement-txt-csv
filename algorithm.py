3505d49ad44400da54f2c668e0a3bf78fb98ebd9
Abdul Ahad <79562127+abdulahadakram@users.noreply.github.com>
2021-05-29 06:03:25 +0500

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -1,14 +1,25 @@
-# 0 Done - Skip first 8 lines
-# 1- Find comma and delete it
+# Things that have been done:
+# 0. Skip first 9 rows
+# 1. Replace commas with `
 # 2. Replace "-" with space
-# 3.  Use regex and remove extra spaces
+# 3. Use regex and remove extra spaces
 # 4. Replace "|" with ","
 # 5. add " and " around the figures with character `
+# 6. replace ` with ,
+# 7. Join cells at the end and make a final csv
+# 8. Account information added at the top
+# 9. Minor bugs fixed
 
-import os
 import re
-import csv
 import numpy as np
+
+
+def add_account_info(temp):
+    file_handler = open("sample.txt", "r", encoding='utf8')
+    temp_str = file_handler.readlines()[0:8]
+    for var in reversed(temp_str):
+        temp.insert(0, var)
+    return temp
 
 
 def test(temp_x, temp_rows, skip_rows=9):
@@ -25,31 +36,42 @@ def test(temp_x, temp_rows, skip_rows=9):
     return False, 0
 
 
-# def convert(lst):
-#     return (lst.split('|'))
-# Read data using pandas
-f = open("sample.txt", "r", encoding='utf8')
-# f=f.read()
-str1 = f.readlines()[9:]
-# print(str1)
-# idf = pd.read_fwf("sample.txt", skiprows=9, header=None,index_col=0)
-# df = pd.DataFrame(idf)
-# df[df.0.str.contains(r'|')]
-rows, do_stuff, str2 = 0, True, []
-for x in str1:
-    do_stuff, rows = test(x, rows)
-    if do_stuff:
+def join_cells(input_str):
+    str3 = []
+    # Start: Code for generating a final string with joined cells
+    concatenated_str = ""
+    for i, temp in reversed(list(enumerate(input_str))):
+        if ',' in temp:
+            my_list = list(temp.split(','))
+            if len(my_list[6]) > 1:
+                my_list[3] += concatenated_str
+                temp_str = ""
+                for string in my_list:
+                    temp_str += string + ','
+                str3.insert(0, temp_str)
+                concatenated_str = ""
+            else:
+                concatenated_str = my_list[3] + ' ' + concatenated_str
+        else:
+            str3.insert(0, temp)
+    # End: Code for generating a final string with joined cells
+    return str3
+
+
+file = open("sample.txt", "r", encoding='utf8')
+read_file = file.readlines()[9:]
+file.close()
+rows, allowed_work, working_str, final_str = 0, True, [], []
+for line in read_file:
+    allowed_work, rows = test(line, rows)
+    if allowed_work:
         continue
-    x = re.sub("-", '', x)
-    x = re.sub(',', '', x)
-    x = re.sub('  ', '', x)
-    x = x.replace('|', ',')
-    x = re.sub('\n', '', x)
-    print(x, end='\n')
-    str2.append(x)
-# df = re.sub(r"-", "", df)
-# print(str2)
-# write back
-# using the save txt
-# from the numpy module
-np.savetxt("output.csv", str2, delimiter=", ", fmt='% s')
+    line = re.sub("-", '', line)
+    line = re.sub(',', '', line)
+    line = re.sub('  ', '', line)
+    line = line.replace('|', ',')
+    line = re.sub('\n', '', line)
+    working_str.append(line)
+final_str = join_cells(working_str)
+final_str = add_account_info(final_str)
+np.savetxt("output.csv", final_str, delimiter=", ", fmt='% s')
